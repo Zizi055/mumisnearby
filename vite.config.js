6d64cfcbf8ebb5151fd1c211226e16f59255a43a
Khalesia <mishel8807@mail.ru>
2026-04-23 11:18:01 +0300

    fix: proper netlify build config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         constructor: resolve(__dirname, 'constructor.html'),
+        lk: resolve(__dirname, 'lk.html'),
       },
     },
   },
