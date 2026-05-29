c0f5f5c22944b405a1378ec54bece5e0aa1a79e1
Khalesia <mishel8807@mail.ru>
2026-05-29 14:17:03 +0300

    fix api url

--- lk/src/api/library.service.js
+++ lk/src/api/library.service.js
@@ -1,17 +1,43 @@
 import { api } from './client';
 
 export async function getLibraryItems(type) {
-  return api.get(`/library?type=${type}`);
+
+  switch (type) {
+
+    case 'fairy_tale':
+
+      return api.get('/content/fairy-tales');
+
+    case 'lullaby':
+
+      return api.get('/content/lullabies');
+
+    case 'therapy':
+
+      return api.get('/content/therapies');
+
+    default:
+
+      return api.get('/content/fairy-tales');
+
+  }
+
 }
 
 export async function generateStory(payload) {
+
   return api.post('/stories/generate', payload);
+
 }
 
 export async function getStory(id) {
+
   return api.get(`/stories/${id}`);
+
 }
 
 export async function toggleFavorite(id) {
+
   return api.post(`/library/${id}/favorite`);
+
 }
