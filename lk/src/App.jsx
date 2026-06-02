a17fa7e8c2f080113b5329ad34d1141ea6930143
Khalesia <mishel8807@mail.ru>
2026-06-03 01:19:55 +0300

    Fix LibraryItem import path

--- lk/src/App.jsx
+++ lk/src/App.jsx
@@ -23,7 +23,7 @@ import Profile from './pages/profile/Profile.jsx';
 import Settings from './pages/settings/Settings.jsx';
 
 import LkLayout from './components/layout/LkLayout';
-import LibraryItem from './pages/library/LibraryItem';
+import LibraryItem from './components/library/LibraryItem';
 function App() {
   return (
     <Routes>
